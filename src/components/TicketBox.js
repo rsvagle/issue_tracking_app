134c35a2fa9e550486072fb5e6300cf29b9cd6a7
rsvagle <rsvagle@gmail.com>
2019-11-29 23:14:09 -0600

    added components to make

---
+++ src/components/TicketBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TicketBox extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TicketBox
---
+++ src/components/layout/TopHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TopHeader extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TopHeader
